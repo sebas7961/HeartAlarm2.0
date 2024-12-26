889c59400840d21e8f170b9a86b6c0aead596d69
Lz1233 <ivanvilchez123@hotmail.com>
2024-12-25 19:09:21 -0500

    Interfaz actualizada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,9 @@ dependencies {
         testImplementation(libs.junit)
         androidTestImplementation(libs.ext.junit)
         androidTestImplementation(libs.espresso.core)
+        implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+        implementation ("com.airbnb.android:lottie:5.2.0")
+
     }
 
 }
--- app/src/main/java/com/example/heartalarm20/view/fragments/MonitoreoFragment.java
+++ app/src/main/java/com/example/heartalarm20/view/fragments/MonitoreoFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.heartalarm20.R;
+
+
 
 public class MonitoreoFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_monitoreo, container, false);
+        // Inflar el diseño del fragmento
+        View view = inflater.inflate(R.layout.fragment_monitoreo, container, false);
+    return view;
     }
 }
